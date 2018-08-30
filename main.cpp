45a797e2622bd916b0a0566bc51815b686fb4ae4
Spencer <morecode@teknik.io>
2018-08-30 14:47:03 -0400

    Unresizable

--- main.cpp
+++ main.cpp
@@ -4,56 +4,58 @@
 
 int main()
 {
-  
-    sf::RenderWindow window(sf::VideoMode(870, 630), "Penguin Paradise");
-    
-    // run the program as long as the window is open 
-    sf::Texture penguinTexture;
-    sf::Texture backTexture;
 
-    penguinTexture.loadFromFile("images/penguin.png");
-    penguinTexture.setSmooth(true);
-    backTexture.loadFromFile("images/background.png");
+	sf::RenderWindow window(sf::VideoMode(870, 630), "Experimental Penguins Rewritten", sf::Style::Titlebar | sf::Style::Close);
 
-    float playScale = 0.2f;
-    sf::Sprite penguin;
-    sf::Sprite background;
+  sf::Image icon;
+  icon.loadFromFile("images/icon.png");
+  window.setIcon(32,32, icon.getPixelsPtr());
+	
+	// run the program as long as the window is open 
+	sf::Texture penguinTexture;
+	sf::Texture backTexture;
 
-    penguin.setTexture(penguinTexture);
-    penguin.setOrigin(sf::Vector2f(329*0.5f, 341*0.5f));
-    penguin.setScale(sf::Vector2f(playScale, playScale));
+	penguinTexture.loadFromFile("images/penguin.png");
+	penguinTexture.setSmooth(true);
+	backTexture.loadFromFile("images/background.png");
 
-    background.setTexture(backTexture);
+	float playScale = 0.2f;
+	sf::Sprite penguin;
+	sf::Sprite background;
 
-    while (window.isOpen())
-    {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
+	penguin.setTexture(penguinTexture);
+	penguin.setOrigin(sf::Vector2f(329 * 0.5f, 341 * 0.5f));
+	penguin.setScale(sf::Vector2f(playScale, playScale));
 
-        while (window.pollEvent(event))
-        {
-          switch (event.type)
-          {
-            case sf::Event::Closed:
-              window.close();
-              break;
-            case sf::Event::MouseButtonPressed:
-              if (event.mouseButton.button == sf::Mouse::Right || event.mouseButton.button == sf::Mouse::Left)
-              {
-                std::cout << "Mouse is at " << event.mouseButton.x << "," << event.mouseButton.y << std::endl;
-                penguin.setPosition(sf::Vector2f(event.mouseButton.x, event.mouseButton.y));
-              }
-            default:
-              break;
-          }
+	background.setTexture(backTexture);
 
-          window.clear();
-          window.draw(background);
-          window.draw(penguin);
-          window.display();
-        }
-    }
+	while (window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
 
-    return 0;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::MouseButtonPressed:
+				if (event.mouseButton.button == sf::Mouse::Right || event.mouseButton.button == sf::Mouse::Left)
+				{
+					penguin.setPosition(sf::Vector2f(event.mouseButton.x, event.mouseButton.y));
+				}
+			default:
+				break;
+			}
+
+			window.clear();
+			window.draw(background);
+			window.draw(penguin);
+			window.display();
+		}
+	}
+
+	return 0;
 }
-
